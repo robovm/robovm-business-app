b920f775660d4a7e4416ae5c12fc33f9f41e87f9
Niklas Therning <niklas@therning.org>
2015-10-21 01:09:37 +0200

    Minor fixes in Reports view.

--- ios/src/main/java/org/robovm/samples/contractr/ios/viewcontrollers/ReportsViewController.java
+++ ios/src/main/java/org/robovm/samples/contractr/ios/viewcontrollers/ReportsViewController.java
@@ -20,6 +20,8 @@ import org.robovm.apple.uikit.UIColor;
 import org.robovm.apple.uikit.UIView;
 import org.robovm.apple.uikit.UIViewController;
 import org.robovm.objc.annotation.CustomClass;
+import org.robovm.objc.annotation.IBOutlet;
+import org.robovm.samples.contractr.ios.IOSColors;
 import org.robovm.samples.contractr.ios.NSLayoutConstraintUtil;
 import org.robovm.samples.contractr.ios.iosplot.PCPieChart;
 import org.robovm.samples.contractr.ios.iosplot.PCPieComponent;
@@ -27,40 +29,29 @@ import org.robovm.samples.contractr.ios.iosplot.PCPieComponent;
 @CustomClass("ReportsViewController")
 public class ReportsViewController extends UIViewController {
 
-    private static final UIColor[] colors = new UIColor[] {
-        UIColor.yellow(),
-        UIColor.red(),
-        UIColor.magenta(),
-        UIColor.cyan(),
-        UIColor.green(),
-        UIColor.brown(),
-        UIColor.orange(),
-        UIColor.blue(),
-        UIColor.purple()
-    };
-    
+    @IBOutlet PCPieChart pieChart;
+
     @Override
     public void viewDidLoad() {
         super.viewDidLoad();
         
-        PCPieChart pieChart = new PCPieChart();
         pieChart.setDiameter(200);
         NSMutableArray<PCPieComponent> components = new NSMutableArray<PCPieComponent>(
-                new PCPieComponent("Apple", 100, colors[0]),
-                new PCPieComponent("Google", 200, colors[1]),
-                new PCPieComponent("Oracle", 200, colors[2])
+                new PCPieComponent("Apple", 100, IOSColors.getClientColor(0)),
+                new PCPieComponent("Google", 200, IOSColors.getClientColor(1)),
+                new PCPieComponent("Oracle", 200, IOSColors.getClientColor(2))
                 );
         pieChart.setComponents(components);
-        pieChart.setTranslatesAutoresizingMaskIntoConstraints(false);
-        
-        UIView rootView = getView();
-        rootView.setBackgroundColor(UIColor.white());
-        rootView.addSubview(pieChart);
-        
-        rootView.addConstraint(NSLayoutConstraintUtil.centerHorizontally(pieChart, rootView, 1.0, 0));
-        rootView.addConstraint(NSLayoutConstraintUtil.centerVertically(pieChart, rootView, 1.0, 0));
-        rootView.addConstraint(NSLayoutConstraintUtil.equalWidth(pieChart, rootView, 1.0, 0));
-        rootView.addConstraint(NSLayoutConstraintUtil.equalHeight(pieChart, rootView, 1.0, 0));
+//        pieChart.setTranslatesAutoresizingMaskIntoConstraints(false);
+//
+//        UIView rootView = getView();
+//        rootView.setBackgroundColor(UIColor.white());
+//        rootView.addSubview(pieChart);
+//
+//        rootView.addConstraint(NSLayoutConstraintUtil.centerHorizontally(pieChart, rootView, 1.0, 0));
+//        rootView.addConstraint(NSLayoutConstraintUtil.centerVertically(pieChart, rootView, 1.0, 0));
+//        rootView.addConstraint(NSLayoutConstraintUtil.equalWidth(pieChart, rootView, 1.0, 0));
+//        rootView.addConstraint(NSLayoutConstraintUtil.equalHeight(pieChart, rootView, 1.0, 0));
     }
     
 }
